8577f5c6ee9deea5d00562fecb232ee4cb025f70
ChaitraKamathG <chaitra.kamath0496@gmail.com>
2015-01-10 23:03:09 +0530

    my first commit
    sic emulator code

---
+++ mnemonicsFunctions.js
@@ -0,0 +1,40 @@
+
+//MUL MNEUMONIC
+function mul(memory){
+	accumulator=accumulator*memory
+}
+
+//OR MNEUMONIC
+function or(memory){
+	accumulator=accumulator|memory
+}	
+
+//STA MNEUMONIC
+function sta(memory){
+  memory=accumulator
+}
+
+//STCH MNEUMONIC
+function stch(memory){
+  memory=accumulator
+}     
+
+//STL MNEUMONIC
+function stl(memory){
+  memory=linkage
+}
+
+//STSW MNEUMONICS
+function stsw(memory){
+  memory=status word
+}  
+
+//STX MNEUMONIC
+function stx(memory){
+  memory=indexRegister
+} 
+
+//SUB MNEUMONIC
+function sub(memory){
+  accumulator=accumulator-memory
+}   
